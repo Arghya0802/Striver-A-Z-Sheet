9441b121c0a67a092b079400bc3d07921c9b3bd3
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-02-19 09:55:52 +0530

    Create Search Insert Position.cpp

---
+++ Binary Search/Binary Search on 1D Arrays/Search Insert Position.cpp
@@ -0,0 +1,37 @@
+// Link: https://leetcode.com/problems/search-insert-position/description/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target)
+    {
+        int n = nums.size() ;
+
+        int low  = 0 ;    
+        int high = n - 1 ;
+
+        int ans = -1 ;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2 ;
+
+            if(nums[mid] == target)
+                return mid ;
+
+            else if(nums[mid] > target)
+            {
+                ans = mid ;
+
+                high = mid - 1 ;
+            }
+
+            else   
+                low = mid + 1 ;
+        }
+
+        return ans == -1 ? n : ans ;
+    }
+};
+
+// Time Complexity:  O(Log N)
+// Space Complexity: O(1)
